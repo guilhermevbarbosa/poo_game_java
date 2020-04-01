4c5faf7bfd5e32685cb14e67445459e49f130aea
Guilherme Vinicius Barbosa <guilhermevbarbosa1999@gmail.com>
2020-04-01 17:12:35 -0300

    Update Dragao.java

--- src/senac/game/combatentes/Dragao.java
+++ src/senac/game/combatentes/Dragao.java
@@ -1,6 +1,6 @@
 package senac.game.combatentes;
 
-public class Dragao extends Combatente{
+public abstract class Dragao extends Combatente{
 	public Dragao(String nome, int vida) {
 		super(nome, 18, vida);
 	}
