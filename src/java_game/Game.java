6072729fdfb627d03c07465a0bbd9a6bf717ce35
Guilherme Vinicius Barbosa <guilhermevbarbosa1999@gmail.com>
2020-04-18 21:43:13 -0300

    combatente factory teste

--- src/java_game/Game.java
+++ src/java_game/Game.java
@@ -1,5 +1,6 @@
 package java_game;
 
+import java.util.Scanner;
 import senac.game.batalhas.Batalha;
 import senac.game.combatentes.Combatente;
 import senac.game.combatentes.dragoes.BloodDragon;
@@ -8,12 +9,17 @@ import senac.game.combatentes.orcs.OrcDaMontanha;
 public class Game {
 
 	public static void main(String[] args) throws InterruptedException {
-		Combatente player1 = new BloodDragon();
-		Combatente player2 = new OrcDaMontanha();
-
-		Batalha batalha = new Batalha(player1, player2);
-		Combatente vencedor = batalha.Batalhar();
-		System.out.println("O vencedor é " + vencedor.getNome());
+		
+		
+		
+		
+		
+//		Combatente player1 = new BloodDragon();
+//		Combatente player2 = new OrcDaMontanha();
+//
+//		Batalha batalha = new Batalha(player1, player2);
+//		Combatente vencedor = batalha.Batalhar();
+//		System.out.println("O vencedor é " + vencedor.getNome());
 	}
 
 }
