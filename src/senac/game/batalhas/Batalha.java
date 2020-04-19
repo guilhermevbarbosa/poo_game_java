760cbc472bb3fb0f2562f0bfe97a77569ff165e7
Guilherme Vinicius Barbosa <guilhermevbarbosa1999@gmail.com>
2020-04-19 16:27:48 -0300

    Classe de batalha separada

--- src/senac/game/batalhas/Batalha.java
+++ src/senac/game/batalhas/Batalha.java
@@ -8,7 +8,8 @@ import senac.game.sorteios.SorteiosBatalha;
 public class Batalha {
 	InputGameSelect inputGameSelect = new InputGameSelect();
 	SorteiosBatalha sorteiosBatalha = new SorteiosBatalha();
-
+	VerificaBatalha verificaBatalha = new VerificaBatalha();
+	
 	private Map<Integer, Combatente> deckJ1;
 	private Map<Integer, Combatente> deckJ2;
 
@@ -28,34 +29,8 @@ public class Batalha {
 		Combatente selecionadoJ2 = deckJ2.get(combatenteSelecionado2);
 
 		while (selecionadoJ1.estaVivo() && selecionadoJ2.estaVivo()) {
-
 			int sorteioJogador = sorteiosBatalha.sorteiaJogadores();
-			System.out.println(sorteioJogador);
-
-			if (sorteioJogador == 1) {
-				int valorAtaque = sorteiosBatalha.sorteiaAtaque(selecionadoJ1.getForca());
-
-				System.out.println(selecionadoJ2.getNome() + " recebeu " + valorAtaque + " de dano");
-				selecionadoJ2.receberAtaque(valorAtaque);
-
-				System.out.println("A vida de " + selecionadoJ1.getNome() + " - " + selecionadoJ1.getVidaAtual());
-				System.out.println("A vida de " + selecionadoJ2.getNome() + " - " + selecionadoJ2.getVidaAtual());
-
-				Thread.sleep(1000);
-				System.out.println("\n");
-			} else {
-				int valorAtaque = sorteiosBatalha.sorteiaAtaque(selecionadoJ2.getForca());
-				
-				System.out.println(selecionadoJ1.getNome() + " recebeu " + valorAtaque + " de dano");
-				selecionadoJ1.receberAtaque(valorAtaque);
-
-				System.out.println("A vida de " + selecionadoJ1.getNome() + " - " + selecionadoJ1.getVidaAtual());
-				System.out.println("A vida de " + selecionadoJ2.getNome() + " - " + selecionadoJ2.getVidaAtual());
-
-				Thread.sleep(1000);
-				System.out.println("\n");
-			}
-
+			verificaBatalha.batalhar(sorteioJogador, selecionadoJ1, selecionadoJ2);
 		}
 
 	}
---
+++ src/senac/game/batalhas/VerificaBatalha.java
@@ -0,0 +1,35 @@
+package senac.game.batalhas;
+
+import senac.game.combatentes.Combatente;
+import senac.game.sorteios.SorteiosBatalha;
+
+public class VerificaBatalha {
+	
+	SorteiosBatalha sorteiosBatalha = new SorteiosBatalha();
+	
+	public void batalhar(int sorteado, Combatente combatente1, Combatente combatente2) throws InterruptedException {
+		if (sorteado == 1) {
+			int valorAtaque = sorteiosBatalha.sorteiaAtaque(combatente1.getForca());
+
+			System.out.println(combatente2.getNome() + " recebeu " + valorAtaque + " de dano");
+			combatente2.receberAtaque(valorAtaque);
+
+			System.out.println("A vida de " + combatente1.getNome() + " - " + combatente1.getVidaAtual());
+			System.out.println("A vida de " + combatente2.getNome() + " - " + combatente2.getVidaAtual());
+
+			Thread.sleep(1000);
+			System.out.println("\n");
+		} else {
+			int valorAtaque = sorteiosBatalha.sorteiaAtaque(combatente2.getForca());
+
+			System.out.println(combatente1.getNome() + " recebeu " + valorAtaque + " de dano");
+			combatente1.receberAtaque(valorAtaque);
+
+			System.out.println("A vida de " + combatente1.getNome() + " - " + combatente1.getVidaAtual());
+			System.out.println("A vida de " + combatente2.getNome() + " - " + combatente2.getVidaAtual());
+
+			Thread.sleep(1000);
+			System.out.println("\n");
+		}
+	}
+}
