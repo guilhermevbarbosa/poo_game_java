0e82f3a6ed8ce01a1732331fe7c6bca99c97d779
Guilherme Vinicius Barbosa <guilhermevbarbosa1999@gmail.com>
2020-04-19 13:59:18 -0300

    separando em classes diferentes os métodos de sorteio

--- src/senac/game/batalhas/Batalha.java
+++ src/senac/game/batalhas/Batalha.java
@@ -1,39 +1,25 @@
 package senac.game.batalhas;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import senac.game.combatentes.Combatente;
 import senac.game.input.InputGameSelect;
+import senac.game.sorteios.SorteiosBatalha;
 
 public class Batalha {
-	private Random rand = new Random();
+	InputGameSelect inputGameSelect = new InputGameSelect();
+	SorteiosBatalha sorteiosBatalha = new SorteiosBatalha();
+	
 	private Map<Integer, Combatente> deckJ1;
 	private Map<Integer, Combatente> deckJ2;
-	InputGameSelect inputGameSelect = new InputGameSelect();
 
 	public Batalha(Map<Integer, Combatente> deckJ1, Map<Integer, Combatente> deckJ2) {
 		this.deckJ1 = deckJ1;
 		this.deckJ2 = deckJ2;
 	}
 
-	private int sorteiaAtaque(int valor) {
-		int ataque = rand.nextInt(valor);
-
-		return ataque;
-	}
-
-	private int sorteiaJogadores() {
-		int jogadorSorteado = rand.nextInt(2);
-
-		return jogadorSorteado;
-	}
-
 	public void Batalhar() throws InterruptedException {
-		int selecionado = sorteiaJogadores();
+		int selecionado = sorteiosBatalha.sorteiaJogadores();
 		inputGameSelect.inputSelecionarCombatente(selecionado, deckJ1, deckJ2);
-
-		
 	}
 
 }
---
+++ src/senac/game/sorteios/SorteiosBatalha.java
@@ -0,0 +1,19 @@
+package senac.game.sorteios;
+
+import java.util.Random;
+
+public class SorteiosBatalha {
+	private Random rand = new Random();
+
+	public int sorteiaAtaque(int valor) {
+		int ataque = rand.nextInt(valor);
+
+		return ataque;
+	}
+
+	public int sorteiaJogadores() {
+		int jogadorSorteado = rand.nextInt(2);
+
+		return jogadorSorteado;
+	}
+}
