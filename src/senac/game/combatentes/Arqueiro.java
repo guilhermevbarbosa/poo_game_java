deffb1e753e925988c62f98057f359dc2d07bd18
Guilherme Vinicius Barbosa <guilherme.barbosa@fri.to>
2020-04-19 21:09:09 -0300

    Bug Fix batalha

--- src/senac/game/combatentes/Arqueiro.java
+++ src/senac/game/combatentes/Arqueiro.java
@@ -1,7 +1,12 @@
 package senac.game.combatentes;
 
 public class Arqueiro extends Combatente{
+	
+	int hash;
+	
 	public Arqueiro() {
 		super("Arqueiro", 10, 100);
+		this.hash = this.hashCode();
 	}
+	
 }
